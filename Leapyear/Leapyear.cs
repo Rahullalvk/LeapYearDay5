2b952c971a2e19b4850ee2c860b4e555902a2f28
Rahulalvk <rahulal432@gmail.com>
2023-05-01 00:55:33 -0700

    Leap year programe to check wheather the year is leap year or not

---
+++ Leapyear/Leapyear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leapyear
+{
+    internal class Leapyear
+    {
+        public static void Displayleapyear()
+        {
+            Console.WriteLine("enter the year");
+            int year = Convert.ToInt32(Console.ReadLine()); 
+            if(year >1000 && year <9999)
+            {
+                if (year % 4 ==0 && (year % 100 != 0 || year % 400 ==0))
+                {
+                    Console.WriteLine("the {0} is a leap year", year);
+                }
+                else
+                {
+                    Console.WriteLine("the {0} is not a leap year", year);
+                }
+            }
+        }
+    }
+}
